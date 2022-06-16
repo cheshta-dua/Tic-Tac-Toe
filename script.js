3885af9b0dcec3fad24d8f32da854bde4ac0864d
cheshta <96103184+cheshta-dua@users.noreply.github.com>
2022-06-16 12:56:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//setting up sounds 
+//for game sounds 
 let audioTurnX = new Audio("others/tingX.mp3");
 let audioTurn0 = new Audio("others/ting0.mp3");
 let audioWin = new Audio("others/win.mp3");
@@ -6,6 +6,7 @@ let audioReset = new Audio("others/reset.mp3");
 
 let turn = "X";
 let gameOver = false;
+let boxes = document.getElementsByClassName("box");
 
 // Function to change turn
 function changeTurn() {
@@ -15,9 +16,11 @@ function changeTurn() {
 }
 
 //Function to check who won
+let counter = 0;
 function checkWin() {
     let boxtexts = document.getElementsByClassName("text");
-    let wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    let wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]; //indexes which user needs to fill in order to win 
+    //if each of these options is filled by the same player (X or 0), then that player wins 
     wins.forEach(e => {
         if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[1]].innerText === boxtexts[e[2]].innerText) && (boxtexts[e[0]].innerText !== "")) {
             document.querySelector(".info").innerText = boxtexts[e[0]].innerText + " won";
@@ -34,7 +37,6 @@ function checkWin() {
 }
 
 //Game logic 
-let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => { //Array.from() creates an array from an object
     var boxtext = element.querySelector(".text"); //get the first element 
     element.addEventListener('click', () => {
@@ -51,8 +53,16 @@ Array.from(boxes).forEach(element => { //Array.from() creates an array from an o
             }
             turn = changeTurn();
             checkWin();
+
             if (!gameOver) {
-                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn; //changes first element of class info 
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+            else { 
+                //users cannot play after someone wins/match draws, they should reset to continue playing 
+                //get all boxes and set the pointer event to none so they become unclickable
+                Array.from(boxes).forEach(element => {
+                    element.style.pointerEvents = "none";
+                })
             }
         }
     })
@@ -69,10 +79,12 @@ reset.addEventListener('click', () => {
         else {
             element.classList.remove("player0");
         }
-
     });
+    Array.from(boxes).forEach(element => {
+        element.style.pointerEvents = "auto"; //resetting the pointer event so that users can play again
+    })
     turn = "X";
-    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn; //first turn will be of X
     gameOver = false;
     audioReset.play();
 })
@@ -88,6 +100,8 @@ ham.addEventListener('click', function () {
 })
 
 //toggle to dark mode
+//this won't keep the btn toggled on refresh, gotta use local/session storage for that 
+//for auto detecting the device mode, use something called 'prefers color scheme'
 const modeBtn = document.querySelector(".switch");
 modeBtn.addEventListener('change', () => {
     document.body.classList.toggle('dark-theme');
