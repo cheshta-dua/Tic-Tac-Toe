b1d05227b137d2a33ef662deabada1f71616b681
cheshta <96103184+cheshta-dua@users.noreply.github.com>
2022-06-17 20:38:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let audioTurnX = new Audio("others/tingX.mp3");
 let audioTurn0 = new Audio("others/ting0.mp3");
 let audioWin = new Audio("others/win.mp3");
-let audioReset = new Audio("others/reset.mp3");
+let audioReset = new Audio("others/reset.wav");
+let audioDraw = new Audio("others/draw.mp3");
 
 let turn = "X";
 let gameOver = false;
@@ -16,12 +17,12 @@ function changeTurn() {
 }
 
 //Function to check who won
-let counter = 0;
 function checkWin() {
     let boxtexts = document.getElementsByClassName("text");
     let wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]; //indexes which user needs to fill in order to win 
     //if each of these options is filled by the same player (X or 0), then that player wins 
     wins.forEach(e => {
+        //if position 0=1=2 or 3=4=5 or... then player wins
         if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[1]].innerText === boxtexts[e[2]].innerText) && (boxtexts[e[0]].innerText !== "")) {
             document.querySelector(".info").innerText = boxtexts[e[0]].innerText + " won";
 
@@ -36,12 +37,15 @@ function checkWin() {
     })
 }
 
-//Game logic 
+//Game logic
+//boxes = document.getElementsByClassName("box"); this returns a collection of elements so we need an array to store
+let counter = 0; //Will help in draw condition 
 Array.from(boxes).forEach(element => { //Array.from() creates an array from an object
-    var boxtext = element.querySelector(".text"); //get the first element 
+    var boxtext = element.querySelector(".text");
     element.addEventListener('click', () => {
-        if (boxtext.innerText === '') {
-            boxtext.innerText = turn;
+        if (boxtext.innerText === '') { //if the box is empty
+            boxtext.innerText = turn; //fill it with X or 0 according to the turn 
+            ++counter; //on each turn, increment the counter. When counter=9, all the boxes will get filled
 
             if (turn === "X") {
                 boxtext.classList.add("playerX");  //for changing color of X (blue X & green 0)
@@ -54,10 +58,18 @@ Array.from(boxes).forEach(element => { //Array.from() creates an array from an o
             turn = changeTurn();
             checkWin();
 
+            //draw condition 
+            if (counter === 9 && !gameOver) {
+                //gameOver condition important cos if the player wins on the very last turn then it shouldn't show "draw"
+                document.getElementsByClassName("info")[0].innerText = "Match draw";
+                audioDraw.play();
+                gameOver = true;
+            }
+
             if (!gameOver) {
                 document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             }
-            else { 
+            else {
                 //users cannot play after someone wins/match draws, they should reset to continue playing 
                 //get all boxes and set the pointer event to none so they become unclickable
                 Array.from(boxes).forEach(element => {
@@ -73,6 +85,7 @@ reset.addEventListener('click', () => {
     let boxtext = document.querySelectorAll(".text");
     Array.from(boxtext).forEach(element => {
         element.innerText = "";
+        //removing the class cos if not, colors get messed up nxt time (like span.text.playerX.player0)
         if (element.className === "playerX") {
             element.classList.remove("playerX");
         }
@@ -86,6 +99,7 @@ reset.addEventListener('click', () => {
     turn = "X";
     document.getElementsByClassName("info")[0].innerText = "Turn for " + turn; //first turn will be of X
     gameOver = false;
+    counter = 0;
     audioReset.play();
 })
 
